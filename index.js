abd1efa90b8fa4443ef08e8843da576a4f31190e
arafat2020 <arafatmannan2019@gmail.com>
2022-07-25 21:38:19 +0600

    set up cors

--- index.js
+++ index.js
@@ -40,14 +40,13 @@ app.use(logoutRouter)
 // port//
 const port = process.env.PORT || 5000;
 const server = http.createServer(app);
-const io = new Server(server, 
-  {
-    cors: {
-      origin: "http://localhost:3000",
-      allowedHeaders: ["my-custom-header"],
-      credentials: true
-    }
-})
+const io = new Server(server, {
+  cors: {
+    origin: "https://chat-phi-teal.vercel.app",
+    allowedHeaders: ["my-custom-header"],
+    credentials: true,
+  },
+});
 io.on('connection', (socket) => {
   console.log('connected to the client')
   socket.on('msgUdate', (res) => {
